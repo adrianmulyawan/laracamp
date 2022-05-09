a9364166f4e7b81ad06987689c25d67adf46b5e0
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-05-09 14:23:09 +0700

    Add Model and Migration for camp_benefits Table

---
+++ database/migrations/2022_05_09_071339_create_camp_benefits_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampBenefitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('camp_id');
+            $table->foreign('camp_id')->references('id')->on('camps')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+}
