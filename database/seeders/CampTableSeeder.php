dbdd045e81ec09ae83a24498adfde1996d3aaed0
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-05-09 19:34:41 +0700

    Create Camp Data Seeders

---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Camp;
+use Illuminate\Database\Seeder;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            [
+                'title' => 'Mahir Web Development',
+                'slug' => 'mahir-web-development',
+                'price' => 127000,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'title' => 'Mahir Data Science',
+                'slug' => 'mahir-data-science',
+                'price' => 152000,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ]
+        ];
+
+        Camp::insert($camps);
+    }
+}
