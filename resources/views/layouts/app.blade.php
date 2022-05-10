57cac8822107e02853cac508e9156680b65e0397
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-05-10 11:21:13 +0700

    Slicing Main Page (Home) to Blade Template Laravel

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    @include('includes.meta')
+
+    @stack('before-style')
+    @include('includes.style')
+    @stack('after-style')
+
+    <title>@yield('title')</title>
+</head>
+
+<body>
+
+    {{-- Navbar --}}
+    @include('includes.navbar')
+
+    @yield('content')
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    {{-- Script --}}
+    @stack('before-script')
+    @include('includes.script')
+    @stack('after-script')
+
+</body>
+
+</html>
