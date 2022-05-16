60ec3f6b2d3111a0c94fe0f7c350097b38e4ca9f
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-05-16 22:03:04 +0700

    Update Checkout Page and CheckeoutRequest.php

--- app/Http/Requests/User/CheckoutRequest.php
+++ app/Http/Requests/User/CheckoutRequest.php
@@ -25,15 +25,14 @@ class CheckoutRequest extends FormRequest
     public function rules()
     {
         // Ambil data bulan dan tahun sekarang
-        $expiredValidation = date('Y-m', time());
+        // $expiredValidation = date('Y-m', time());
 
         return [
             'name' => 'required|string|min:3',
             'email' => 'required|email|unique:users,email,'.Auth::user()->id.',id',
             'occupation' => 'required|string',
-            'card_number' => 'required|numeric|digits_between:8,16',
-            'expired' => 'required|date|date_format:Y-m|after_or_equal:'.$expiredValidation,
-            'cvc' => 'required|numeric|digits:3'
+            'phone' => 'required|string|max:20',
+            'address' => 'required|string',
         ];
     }
 }
