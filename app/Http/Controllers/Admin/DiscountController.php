9f3086fb6093dbae47d1cd09447b304e1778adf7
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-05-18 14:52:26 +0700

    Create Function Store() for Add New Data Discount

--- app/Http/Controllers/Admin/DiscountController.php
+++ app/Http/Controllers/Admin/DiscountController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\DiscountRequest;
 use App\Models\Discount;
 use Illuminate\Http\Request;
 
@@ -35,9 +36,15 @@ class DiscountController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(DiscountRequest $request)
     {
-        //
+        $data = $request->all();
+        Discount::create($data);
+
+        # Tambah Flash Message
+        $request->session()->flash('success', 'A new discount has been created');
+        
+        return redirect()->route('discount.index');
     }
 
     /**
